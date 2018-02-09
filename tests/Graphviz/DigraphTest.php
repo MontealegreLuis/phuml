5b3f9308baa05a32f74bd218cf55dae11a0f2280
Luis Montealegre <montealegreluis@gmail.com>
2018-02-08 21:28:08 -0600

    test: Create more thorough examples for the Digraph class

--- tests/Graphviz/DigraphTest.php
+++ tests/Graphviz/DigraphTest.php
@@ -8,6 +8,7 @@
 namespace PhUml\Graphviz;
 
 use PHPUnit\Framework\TestCase;
+use PhUml\Code\Attributes\Attribute;
 use PhUml\Code\Methods\Method;
 use PhUml\Code\Variables\TypeDeclaration;
 use PhUml\Code\Variables\Variable;
@@ -21,74 +22,117 @@ class DigraphTest extends TestCase
     use ProvidesNumericIds;
 
     /** @test */
-    function it_can_be_represented_as_dot_language_from_a_structure_with_one_definition()
+    function its_dot_language_representation_contains_an_id_and_basic_display_settings()
     {
-        $labelBuilder = new ClassNameLabelBuilder();
         $digraph = new Digraph();
-
-        $class = new NumericIdClass('TestClass');
-        $digraph->add([new Node($class, $labelBuilder->forClass($class))]);
 
         $dotLanguage = $digraph->toDotLanguage();
 
         $this->assertRegExp('/^digraph "([0-9a-f]){40}"/', $dotLanguage);
-        $this->assertStringEndsWith(' {
-splines = true;
+        $this->assertContains('splines = true;
 overlap = false;
-mindist = 0.6;
-"101" [label=<<table><tr><td>TestClass</td></tr></table>> shape=plaintext]
-}', $dotLanguage);
+mindist = 0.6;', $dotLanguage);
     }
 
     /** @test */
-    function it_can_be_represented_as_dot_language_from_a_structure_with_several_definitions()
+    function it_represents_a_single_definition_as_dot_language()
     {
         $labelBuilder = new ClassNameLabelBuilder();
+        $class = new NumericIdClass('TestClass');
         $digraph = new Digraph();
+        $digraph->add([new Node($class, $labelBuilder->forClass($class))]);
 
-        $parentInterface = new NumericIdInterface('ParentInterface');
-        $childInterface = new NumericIdInterface('ChildInterface', [], [], $parentInterface);
-        $anotherInterface = new NumericIdInterface('AnotherInterface');
+        $dotLanguage = $digraph->toDotLanguage();
+
+        $this->assertContains('"101" [label=<<table><tr><td>TestClass</td></tr></table>> shape=plaintext]', $dotLanguage);
+    }
+
+    /** @test */
+    function it_represents_inheritance_as_dot_language()
+    {
+        $labelBuilder = new ClassNameLabelBuilder();
         $parentClass = new NumericIdClass('ParentClass');
-        $referenceClass = new NumericIdClass('AReference');
-        $testClass = new NumericIdClass('TestClass', [], [], [
-            Method::public('__construct', [
-                Variable::declaredWith('aReference', TypeDeclaration::from('AReference'))
-            ])
-        ], [$childInterface, $anotherInterface], $parentClass);
-
+        $class = new NumericIdClass('TestClass', [], [], [], [], $parentClass);
+        $digraph = new Digraph();
         $digraph->add([
-            new Node($referenceClass, $labelBuilder->forClass($referenceClass)),
             new Node($parentClass, $labelBuilder->forClass($parentClass)),
-            Edge::association($referenceClass, $testClass),
-            new Node($testClass, $labelBuilder->forClass($testClass)),
-            Edge::inheritance($parentClass, $testClass),
-            Edge::implementation($childInterface, $testClass),
-            Edge::implementation($anotherInterface, $testClass),
-            new Node($parentInterface, $labelBuilder->forInterface($parentInterface)),
-            new Node($childInterface, $labelBuilder->forInterface($childInterface)),
-            Edge::inheritance($parentInterface, $childInterface),
+            new Node($class, $labelBuilder->forClass($class)),
+            Edge::inheritance($parentClass, $class),
+        ]);
+
+        $dotLanguage = $digraph->toDotLanguage();
+
+        $this->assertContains('"101" [label=<<table><tr><td>ParentClass</td></tr></table>> shape=plaintext]', $dotLanguage);
+        $this->assertContains('"102" [label=<<table><tr><td>TestClass</td></tr></table>> shape=plaintext]', $dotLanguage);
+        $this->assertContains('"101" -> "102" [dir=back arrowtail=empty style=solid]', $dotLanguage);
+    }
+
+    /** @test */
+    function it_represents_interfaces_implementations_as_dot_language()
+    {
+        $labelBuilder = new ClassNameLabelBuilder();
+        $anInterface = new NumericIdInterface('AnInterface');
+        $anotherInterface = new NumericIdInterface('AnotherInterface');
+        $class = new NumericIdClass('TestClass', [], [], [], [$anInterface, $anotherInterface]);
+        $digraph = new Digraph();
+        $digraph->add([
+            new Node($class, $labelBuilder->forClass($class)),
+            Edge::implementation($anInterface, $class),
+            Edge::implementation($anotherInterface, $class),
+            new Node($anInterface, $labelBuilder->forInterface($anInterface)),
             new Node($anotherInterface, $labelBuilder->forInterface($anotherInterface)),
         ]);
 
         $dotLanguage = $digraph->toDotLanguage();
 
-        $this->assertRegExp('/^digraph "([0-9a-f]){40}"/', $dotLanguage);
-        $this->assertStringEndsWith(' {
-splines = true;
-overlap = false;
-mindist = 0.6;
-"102" [label=<<table><tr><td>AReference</td></tr></table>> shape=plaintext]
-"101" [label=<<table><tr><td>ParentClass</td></tr></table>> shape=plaintext]
-"102" -> "103" [dir=back arrowtail=none style=dashed]
-"103" [label=<<table><tr><td>TestClass</td></tr></table>> shape=plaintext]
-"101" -> "103" [dir=back arrowtail=empty style=solid]
-"2" -> "103" [dir=back arrowtail=normal style=dashed]
-"3" -> "103" [dir=back arrowtail=normal style=dashed]
-"1" [label=<<table><tr><td>ParentInterface</td></tr></table>> shape=plaintext]
-"2" [label=<<table><tr><td>ChildInterface</td></tr></table>> shape=plaintext]
-"1" -> "2" [dir=back arrowtail=empty style=solid]
-"3" [label=<<table><tr><td>AnotherInterface</td></tr></table>> shape=plaintext]
-}', $dotLanguage);
+        $this->assertContains('"101" [label=<<table><tr><td>TestClass</td></tr></table>> shape=plaintext]', $dotLanguage);
+        $this->assertContains('"1" [label=<<table><tr><td>AnInterface</td></tr></table>> shape=plaintext]', $dotLanguage);
+        $this->assertContains('"2" [label=<<table><tr><td>AnotherInterface</td></tr></table>> shape=plaintext]', $dotLanguage);
+        $this->assertContains('"1" -> "101" [dir=back arrowtail=normal style=dashed]', $dotLanguage);
+        $this->assertContains('"2" -> "101" [dir=back arrowtail=normal style=dashed]', $dotLanguage);
+    }
+
+    /** @test */
+    function it_represents_constructor_dependencies_as_associations_in_dot_language()
+    {
+        $labelBuilder = new ClassNameLabelBuilder();
+        $referenceClass = new NumericIdClass('AReference');
+        $testClass = new NumericIdClass('TestClass', [], [], [
+            Method::public('__construct', [
+                Variable::declaredWith('aReference', TypeDeclaration::from('AReference'))
+            ])
+        ]);
+        $digraph = new Digraph();
+        $digraph->add([
+            new Node($referenceClass, $labelBuilder->forClass($referenceClass)),
+            Edge::association($referenceClass, $testClass),
+            new Node($testClass, $labelBuilder->forClass($testClass)),
+        ]);
+
+        $dotLanguage = $digraph->toDotLanguage();
+
+        $this->assertContains('"101" [label=<<table><tr><td>AReference</td></tr></table>> shape=plaintext]', $dotLanguage);
+        $this->assertContains('"102" [label=<<table><tr><td>TestClass</td></tr></table>> shape=plaintext]', $dotLanguage);
+        $this->assertContains('"101" -> "102" [dir=back arrowtail=none style=dashed]', $dotLanguage);
+    }
+
+    /** @test */
+    function it_represents_class_attributes_as_associations_in_dot_language()
+    {
+        $labelBuilder = new ClassNameLabelBuilder();
+        $referenceClass = new NumericIdClass('AReference');
+        $testClass = new NumericIdClass('TestClass', [], [
+            Attribute::private('$aReference', TypeDeclaration::from('AReference'))
+        ], []);
+        $digraph = new Digraph();
+        $digraph->add([
+            new Node($referenceClass, $labelBuilder->forClass($referenceClass)),
+            Edge::association($referenceClass, $testClass),
+            new Node($testClass, $labelBuilder->forClass($testClass)),
+        ]);
+
+        $dotLanguage = $digraph->toDotLanguage();
+
+        $this->assertContains('"101" [label=<<table><tr><td>AReference</td></tr></table>> shape=plaintext]', $dotLanguage);
     }
 }
