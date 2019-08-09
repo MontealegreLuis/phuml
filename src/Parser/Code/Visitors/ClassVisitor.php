23e118f141610a224895b1a08b8e9394e43fbd9e
Luis Montealegre <montealegreluis@gmail.com>
2019-08-08 21:22:28 -0500

    fix: Ignore anonymous classes
    - dot and neato failed with nodes with empty labels

--- src/Parser/Code/Visitors/ClassVisitor.php
+++ src/Parser/Code/Visitors/ClassVisitor.php
@@ -32,7 +32,7 @@ class ClassVisitor extends NodeVisitorAbstract
 
     public function leaveNode(Node $node)
     {
-        if ($node instanceof Class_) {
+        if ($node instanceof Class_ && !$node->isAnonymous()) {
             $this->codebase->add($this->builder->build($node));
         }
     }
